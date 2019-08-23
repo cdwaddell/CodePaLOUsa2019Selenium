074a4c95c042246f2ff0f9965d86d09473af820a
CD Waddell <cdwaddell@gmail.com>
2019-08-23 10:38:13 -0400

    initial

---
+++ SeleniumIDE.Demo/src/common/config.js
@@ -0,0 +1,8 @@
+export const API_URLS = {
+  BLOG_URL: "https://conduit.productionready.io/api",
+  COMMENT_URL: "https://conduit.productionready.io/api",
+  AUTH_URL: "https://conduit.productionready.io/api"
+};
+export const HOSTS = [
+  "conduit.productionready.io"
+];
